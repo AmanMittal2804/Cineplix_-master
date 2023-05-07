2553787aabde47681c13152e157f95847c82b3af
AmanMittal2804 <mittalaman2804@gmail.com>
2023-05-07 19:46:31 +0530

    LoginCommit

--- src/components/Log/Log.jsx
+++ src/components/Log/Log.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
+import './Log.css'
 import userEvent from '@testing-library/user-event';
 import { Cred } from './Log.styled';
 
@@ -9,11 +10,11 @@ export default function Log() {
   return (
     <div>
       {isAuthenticated && <p>{user.name}</p>}
-        { isAuthenticated ? <Cred onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+        { isAuthenticated ? <button className="log" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
           Log Out
-        </Cred>
+        </button>
         :
-        (<Cred onClick={() => loginWithRedirect()}>Log In</Cred>)}
+        (<button className="log" onClick={() => loginWithRedirect()}>Log In</button>)}
     </div>
   )
 }
